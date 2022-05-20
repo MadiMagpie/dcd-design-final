8b7f8d4fae031c863f1ab178cb6c22e72eb01910
Roncyboy <tpaiement@my.bcit.ca>
2022-05-20 05:05:29 -0700

    swiper updates :)

--- timbertale/pages/choose/index.js
+++ timbertale/pages/choose/index.js
@@ -3,16 +3,11 @@ import { ChooseTitle, ChooseInfo } from '../../comps/Display/choosecards';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { FSBackground, Heading, Wrapper, RowWrapper} from "../../comps/Display";
 import { useRouter } from 'next/router';
-import Image from 'next/image';
-import OldGrowth  from '../../public/Cards_OldGrowth-01.jpg'
-import Sumatran  from '../../public/Cards_Sumatran-01.jpg'
-import Amazon from '../../public/Cards_Amazon-01.jpg'
-import { SettingsBackdrop, SetButton, SettingsModal } from '../../comps/Settings';
 import 'swiper/css';
 import { useState } from 'react';
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
+import { Navigation, Pagination } from "swiper";
  
 export default () => {
   const [settingsOpen, setSettingsOpen] = useState(false);
@@ -29,20 +24,17 @@ export default () => {
    <div style={{background:"#999529", height:"100vh"}}>
      <FSBackground backgroundcolor = "#999529" height = "auto">
       <BackButton onClick={()=>r.back}></BackButton>
-      <SetButton onClick = {settingsHandler}><img src='/settingswhite.svg'/></SetButton>
     <Wrapper margintop="4em" marginbottom = "3em">
       <Heading color="#FFFFFF">Select Your Forest</Heading>
       <ChooseInfo>Forests are important and grow in many places around the world. They are an ecosystem which includes many plants and animals.</ChooseInfo>
     </Wrapper></FSBackground>
-    {settingsOpen && <SettingsModal onClick= {closeSettingsHandler}/>}
-    {settingsOpen && <SettingsBackdrop onClick = {closeSettingsHandler}/>}
    <Swiper
-     spaceBetween={10}
-     slidesPerView={3}
+     spaceBetween={90}
+     slidesPerView={2.5}
      centeredSlides={true}
-     loop={true}
      grabCursor={true}
      navigation={true}
+     loop={true}
      pagination={{dynamicBullets:true,}}
      modules={[Pagination, Navigation]}
      onSlideChange={() => console.log('slide change')}
@@ -51,28 +43,16 @@ export default () => {
  >  
     <SwiperSlide>
 
-     <Image src={OldGrowth}
-       alt='oldGrowth'
-       width={300}
-       height={600}
-      />
-      <ShortButton marginleft={"10px"} onClick={()=>r.push("/old_growth/howto")}>Start</ShortButton>
+      <img src='Cards_OldGrowth-01.jpg' alt='old growth'></img>
+      <LongButton size = "1em" onClick={()=>r.push("/old_growth/howto")}>Start</LongButton>
      </SwiperSlide>
      <SwiperSlide>
-       <Image src={Amazon}
-       alt='amazon'
-       width={300}
-       height={600}
-      />
-      <ShortButton marginleft={"10px"} onClick={()=>r.push("/Amazon/howto")}>Start</ShortButton>
+      <img src='Cards_Amazon-01.jpg' alt='Amazon'></img>
+      <LongButton size = "1em" onClick={()=>r.push("/Amazon/howto")}>Start</LongButton>
      </SwiperSlide>
      <SwiperSlide>
-      <Image src={Sumatran} 
-       alt='Sumatran'
-       width={300}
-       height={600}
-        />
-        <ShortButton marginleft={"10px"} onClick={()=>r.push("/Sumatran/howto")}>Start</ShortButton>
+      <img src='Cards_Sumatran-01.jpg' alt='Sumatran'></img>
+        <LongButton size = "1em" onClick={()=>r.push("/Sumatran/howto")}>Start</LongButton>
      </SwiperSlide>
    </Swiper>
   
--- timbertale/pages/old_growth/endWin.js
+++ timbertale/pages/old_growth/endWin.js
@@ -1,9 +1,23 @@
-import { ImgBackground, Heading } from "../../comps/Display";
+import { useRouter } from "next/router";
+import { LongButton } from "../../comps/Button";
+import { ImgBackground, Heading, Desc, Wrapper, Box } from "../../comps/Display";
+import { motion } from "framer-motion";
 
 export default function youWon(){
+        const r = useRouter();
         return(
                 <ImgBackground background = "/OldGrowth_CompleteFinalGrowCongrats-04.svg">
-                        <Heading> you won.</Heading>        
+                        <Box as={motion.div} 
+                initial ={{y:-300}} 
+                animate = {{y:-50}} 
+                transiton={{type: "spring", stiffness: 700, damping: 60}} width = "75%">
+                        <Wrapper>
+                        <Heading color = "#EB6A00">Congratulations!</Heading>
+                        <Desc weight="500">You beat the lumberjack!</Desc>
+                        <Desc padding ="1em">By answering all 10 questions correct, you grew the tallest tree in the BC Old Growth forest at 315 ft tall.</Desc>
+                        <LongButton size = "1em" marginbottom = "1em" onClick={()=>r.push("/")}>Home</LongButton>
+                        </Wrapper>
+                        </Box>        
                 </ImgBackground>
         )
 }
