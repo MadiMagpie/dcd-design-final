61c5137a428d56020f14a6d179f508e97a903785
MadiMagpie <mcafikirwin@gmail.com>
2022-05-19 22:05:27 -0700

    changes routing

--- timbertale/data/old_growth.js
+++ timbertale/data/old_growth.js
@@ -2,93 +2,93 @@ export const qs = [
     {   
         title:"How old is BC's oldest tree?",
         choices:[
-            {choice: "1835 Years", isCorrect: true}, 
-            {choice: "683 Years", isCorrect: false},
-            {choice: "948 Years" , isCorrect: false},
-            {choice: "1362 Years" , isCorrect: false},
+            {choice: "1835 Years", isCorrect: true, clr: "#999529"}, 
+            {choice: "683 Years", isCorrect: false, clr: "#7D4D2B"},
+            {choice: "948 Years" , isCorrect: false, clr: "#7D4D2B"},
+            {choice: "1362 Years" , isCorrect: false, clr: "#7D4D2B"},
          ],
         // answer:choices[0]
     },   
     {   
         title:"What percent of BCs forests are old growth?",
         choices:[
-            {choice: "46%",  isCorrect: false}, 
-            {choice:"12%", isCorrect: false}, 
-            {choice:"20%", isCorrect: true}, 
-            {choice:"29%", isCorrect: false},
+            {choice: "46%",  isCorrect: false, clr: "#7D4D2B"}, 
+            {choice:"12%", isCorrect: false, clr: "#7D4D2B"}, 
+            {choice:"20%", isCorrect: true, clr: "#999529"}, 
+            {choice:"29%", isCorrect: false, clr: "#7D4D2B"},
         ],
         // answer:choices[2]
     },
     {   
         title:"What is the main tree type that makes up the old growth forests?",
         choices:[
-            {choice: "Hemlocks", isCorrect: false}, 
-            {choice:"Cedars", isCorrect: false}, 
-            {choice:"Spruces", isCorrect: false}, 
-            {choice:"Firs", isCorrect: true}],
+            {choice: "Hemlocks", isCorrect: false, clr: "#7D4D2B"}, 
+            {choice:"Cedars", isCorrect: false, clr: "#7D4D2B"}, 
+            {choice:"Spruces", isCorrect: false, clr: "#7D4D2B"}, 
+            {choice:"Firs", isCorrect: true, clr: "#999529"}],
         // answer:choices[3]
     },
     {   
         title:"How wide is the widest tree in BCs old growth forest?",
         choices:[
-            {choice:"12ft", isCorrect: false}, 
-            {choice:"60ft", isCorrect: true}, 
-            {choice:"30ft", isCorrect: false}, 
-            {choice:"80ft", isCorrect: false}],
+            {choice:"12ft", isCorrect: false, clr: "#7D4D2B"}, 
+            {choice:"60ft", isCorrect: true, clr: "#999529"}, 
+            {choice:"30ft", isCorrect: false, clr: "#7D4D2B"}, 
+            {choice:"80ft", isCorrect: false, clr: "#7D4D2B"}],
         // answer:choices[1] 3.6m, 18.3m, 9.1m, 24.4m
     },
     {   
         title:"What percentage of BC original old growth is protected?",
         choices:[
-            {choice:"15%", isCorrect: false}, 
-            {choice:"32%", isCorrect: false}, 
-            {choice:"21%", isCorrect: false}, 
-            {choice:"6%", isCorrect: true}],
+            {choice:"15%", isCorrect: false, clr: "#7D4D2B"}, 
+            {choice:"32%", isCorrect: false, clr: "#7D4D2B"}, 
+            {choice:"21%", isCorrect: false, clr: "#7D4D2B"}, 
+            {choice:"6%", isCorrect: true, clr: "#999529"}],
         // answer:choices[3]
     },
     {   
         title:"How many hectares of forest are logged anually?",
         choices:[
-            {choice:"140,000", isCorrect: true},
-            {choice:"90,000", isCorrect: false},
-            {choice:"120,000", isCorrect: false},
-            {choice:"50,000", isCorrect: false}],
+            {choice:"140,000", isCorrect: true, clr: "#999529"},
+            {choice:"90,000", isCorrect: false, clr: "#7D4D2B"},
+            {choice:"120,000", isCorrect: false, clr: "#7D4D2B"},
+            {choice:"50,000", isCorrect: false, clr: "#7D4D2B"}],
         // answer:choices[0]
     },
     {   
         title:"How many species rely on BC's old growth forests?",
         choices:[
-            {choice:"0-150", isCorrect: false},
-            {choice:"150-300", isCorrect: false}, 
-            {choice:"300-400", isCorrect: false}, 
-            {choice:"400+", isCorrect: true}],
+            {choice:"0-150", isCorrect: false, clr: "#7D4D2B"},
+            {choice:"150-300", isCorrect: false, clr: "#7D4D2B"}, 
+            {choice:"300-400", isCorrect: false, clr: "#7D4D2B"}, 
+            {choice:"400+", isCorrect: true, clr: "#999529"}],
         // answer:choices[3]
     },
     {   
         title:"How many years for a forest to be considered old growth in BC?",
         choices:[
-            {choice:"250 Years", isCorrect: true}, 
-            {choice:"100 Years", isCorrect: false}, 
-            {choice:"200 Years", isCorrect: false}, 
-            {choice:"150 Years", isCorrect: false}],
+            {choice:"250 Years", isCorrect: true, clr: "#999529"}, 
+            {choice:"100 Years", isCorrect: false, clr: "#7D4D2B"}, 
+            {choice:"200 Years", isCorrect: false, clr: "#7D4D2B"}, 
+            {choice:"150 Years", isCorrect: false, clr: "#7D4D2B"}],
         // answer:choices[0]
     },
     {   
         title:"Where are BCs largest old growth forests located?",
         choices:[
-            {choice:"Northern", isCorrect: false}, 
-            {choice:"Van Island", isCorrect: true}, 
-            {choice:"Okanagan", isCorrect: false}, 
-            {choice:"Metro Van", isCorrect: false}],
+            {choice:"Northern", isCorrect: false, clr: "#7D4D2B"}, 
+            {choice:"Van Island", isCorrect: true, clr: "#999529"}, 
+            {choice:"Okanagan", isCorrect: false, clr: "#7D4D2B"}, 
+            {choice:"Metro Van", isCorrect: false, clr: "#7D4D2B"}],
         // answer:choices[1]
     },
     {   
         title:"Tallest Tree in BCs old growth?",
         choices:[
-            {choice:"260ft", isCorrect: false}, 
-            {choice:"190ft", isCorrect: false}, 
-            {choice:"315ft", isCorrect: true}, 
-            {choice:"410ft", isCorrect: false}],
+            {choice:"260ft", isCorrect: false, clr: "#7D4D2B"}, 
+            {choice:"190ft", isCorrect: false, clr: "#7D4D2B"}, 
+            {choice:"315ft", isCorrect: true, clr: "#999529"}, 
+            {choice:"410ft", isCorrect: false, clr: "#7D4D2B"}],
         // answer:choices[2]
     },
     ];
